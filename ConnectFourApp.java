056304ba9ed9392d66a4c6deafdbf32517bbbd74
Martine <mart.iii.ne@hotmail.com>
2017-03-01 14:25:22 +0100

    Initial commit

---
+++ ConnectFourApp.java
@@ -0,0 +1,11 @@
+package ConnectFour;
+
+/**
+ * Created by martineevjen on 01.03.2017.
+ */
+public class ConnectFourApp {
+    public static void main(String[] args) {
+        System.out.println("Test");
+    }
+}
+
