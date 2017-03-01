ad5f94e7b9958f7a508b4656defe31258f05b4b0
Martine <mart.iii.ne@hotmail.com>
2017-03-01 14:28:40 +0100

    test

--- ConnectFourApp.java
+++ ConnectFourApp.java
@@ -1,11 +1,17 @@
 package ConnectFour;
 
+import javafx.application.Application;
+
 /**
  * Created by martineevjen on 01.03.2017.
  */
-public class ConnectFourApp {
-    public static void main(String[] args) {
-        System.out.println("Test");
-    }
+public class ConnectFourApp extends Application{
+
+    private static final int TILE_SIZE = 80;
+    private static final int COULMNS = 7;
+    private static final int ROWS = 6;
+
+
 }
+
 
